0a1a89a8fd49f7d09f16175be6e10d85918d5d96
causeThenEffect <songzhipeng123456@gmail.com>
2021-12-17 18:14:27 +0800

    拦截器注册

--- basic-starter/src/main/java/com/cause/basic/config/WebConfig.java
+++ basic-starter/src/main/java/com/cause/basic/config/WebConfig.java
@@ -3,19 +3,16 @@ package com.cause.basic.config;
 import com.cause.basic.intercepter.UserHeaderIntercepter;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author cause
  * @date 2021/12/10
- * todo WebMvcAutoConfiguration 实现原理了解一下，ConditionalOnMissingBean什么意思
- *
- * 继承了 WebMvcAutoConfiguration
  */
-//@Configuration
-//@EnableWebMvc
-public class WebConfig extends WebMvcAutoConfiguration implements WebMvcConfigurer {
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
 
   @Bean
   public UserHeaderIntercepter userHeaderIntercepter() {
